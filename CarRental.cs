90118d2ef8b3555a11539812cdbc82308db060ce
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-12-05 00:04:41 -0300

    Add files via upload
    Entre com os dados da locação:
    modelo: Civic
    Check-in (dd/MM/aaaa hs:ms): 25/06/2018 10:30
    Check-out (dd/MM/aaa hs:ms): 25/06/2018 14:40
    Entre com o preço por hora: 10.00
    Entre com o preço por dia: 130.00
    FATURA:
    Pagamento Básico: 50.00
    Taxa: 10.00
    Pagamento Total: 60.00
    Cálculos:
    Duração = (25/06/2018 14:40) - (25/06/2018 10:30) = 4:10 = 5 hours
    Pagamento Básico = 5 * 10 = 50
    Taxa = 50 * 20% = 50 * 0.2 = 10

---
+++ CarRental.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using POO_TarefaInterfaces.Entities;
+
+namespace POO_TarefaInterfaces.Entities
+{
+    class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Vehicle Vehicle { get; private set; }
+        public Invoice Invoice { get; set; }
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null;
+        }
+    }
+}
